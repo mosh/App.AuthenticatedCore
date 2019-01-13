ec8637c3db93e2da48ff27d19be60f19a64fef2a
John Moshakis <john.moshakis@firmex.com>
2019-01-13 01:02:33 -0500

    Refactoring to allow common statup

--- iOSApp.AuthenticatedCore/AuthenticatedServiceBase.pas
+++ iOSApp.AuthenticatedCore/AuthenticatedServiceBase.pas
@@ -6,6 +6,8 @@ uses
   iOSApp.Core,
   iOSApp.AuthenticatedCore.Models,
   iOSApp.AuthenticatedCore.Storage,
+  Moshine.Foundation,
+  RemObjects.Elements.RTL,
   UIKit;
 
 type
@@ -58,17 +60,151 @@ type
       begin
         exit (UIApplication.sharedApplication.&delegate) as AuthenticationAppDelegate;
       end;
+
   protected
+
     property AuthenticationService:AuthenticationService read
       begin
         exit AppDelegate.AuthenticationService;
       end;
+
+    method executeBlock(someBlock:block):operationTypesEnumeration;
+    begin
+      try
+        someBlock;
+        exit operationTypesEnumeration.completed;
+      except
+        on a:AuthenticationRequiredException do
+        begin
+          exit operationTypesEnumeration.authenticationRequired;
+        end;
+        on e:NSException do
+        begin
+          exit operationTypesEnumeration.exception;
+        end;
+      end;
+    end;
+
+
+    method Handle(someCode:block; initiatedAction:InitiatedActionEnumeration := InitiatedActionEnumeration.Unknown);
+    begin
+
+      var outerExecutionBlock: NSBlockOperation := NSBlockOperation.blockOperationWithBlock(method()
+      begin
+
+        var value := AccessToken;
+
+        if(String.IsNullOrEmpty(value))then
+        begin
+
+          NSOperationQueue.mainQueue().addOperationWithBlock(method()
+          begin
+            &delegate:OnNotAuthorized(initiatedAction);
+          end);
+        end
+        else
+        begin
+
+          try
+            someCode;
+
+          except
+            on a:AuthenticationRequiredException do
+            begin
+              NSOperationQueue.mainQueue().addOperationWithBlock(method()
+              begin
+                &delegate:OnNotAuthorized(initiatedAction);
+              end);
+            end;
+            on h:HttpStatusCodeException do
+            begin
+
+              NSOperationQueue.mainQueue().addOperationWithBlock(method()
+              begin
+                &delegate:OnError(h);
+              end);
+
+
+            end;
+            on e:NSException do
+            begin
+
+              NSOperationQueue.mainQueue().addOperationWithBlock(method()
+              begin
+                &delegate:OnError(e);
+              end);
+
+            end;
+          end;
+
+        end;
+
+      end);
+
+      workerQueue.addOperation(outerExecutionBlock);
+
+    end;
+
+    method anyExceptions(values:array of Integer):Boolean;
+    begin
+      for each value in values do
+        begin
+        if value = operationTypesEnumeration.exception then
+        begin
+          exit true;
+        end;
+      end;
+      exit false;
+
+    end;
+
+    method anyCantContinue(values:array of Integer):Boolean;
+    begin
+      for each value in values do
+        begin
+        if value = operationTypesEnumeration.UnableToContinue then
+        begin
+          exit true;
+        end;
+      end;
+      exit false;
+
+    end;
+
+
+    method anyAuthenticationRequired(values:array of Integer):Boolean;
+    begin
+      for each value in values do
+      begin
+        if value = operationTypesEnumeration.authenticationRequired then
+        begin
+          exit true;
+        end;
+      end;
+      exit false;
+    end;
+
+
+    method Ready:Boolean;
+    begin
+      exit assigned(AccessToken) and (AccessToken.Length > 0);
+    end;
 
 
   public
     property &delegate:IServiceEvents;
 
     property Storage:AuthenticatedStorageBase read;
+
+    method currentAuthenticated:Authenticated;
+    begin
+      exit Storage.AuthenticatedUser;
+    end;
+
+    property localUser:Boolean read
+      begin
+        exit assigned(Storage.AuthenticatedUser);
+      end;
 
 
     property AccessToken:String read
@@ -132,6 +268,103 @@ type
         self.delegate:OnAuthorized;
       end;
     end;
+
+    method Startup(results:array of Integer; callback:StartupDelegate; buildStartup:BuildStartupDelegate; reload:Boolean);
+    begin
+
+
+      var outerExecutionBlock: NSBlockOperation := NSBlockOperation.blockOperationWithBlock(method
+      begin
+
+        var auth := Storage.AuthenticatedUser;
+
+        if ((not assigned(auth)) and (offline))then
+        begin
+
+          NSOperationQueue.mainQueue().addOperationWithBlock(method()
+          begin
+            var e:= new NoNetworkConnectionException withName('ProxyException') reason('Authentication required but no network connection') userInfo(nil) ;
+            &delegate:OnError(e);
+          end);
+
+          for each &result in results do
+            begin
+            &result := operationTypesEnumeration.UnableToContinue;
+            end;
+
+        end
+        else if((not assigned(auth)) or (assigned(auth) and reload ))then
+        begin
+
+          var assignedAccessToken := self.AuthenticationService.AccessToken;
+
+          if(String.IsNullOrEmpty(assignedAccessToken))then
+          begin
+            NSLog('No access token not requesting data');
+
+            for each &result in results do
+              begin
+              &result := operationTypesEnumeration.authenticationRequired;
+              end;
+
+          end
+          else
+          begin
+
+            var innerExecutionBlock := buildStartup();
+
+            var innerQueue: NSOperationQueue := new NSOperationQueue();
+            innerQueue.addOperation(innerExecutionBlock);
+            innerQueue.waitUntilAllOperationsAreFinished;
+
+          end;
+
+        end
+        else
+        begin
+          NSLog('Found Auth row');
+
+          for each &result in results do
+            begin
+            &result := operationTypesEnumeration.completed;
+            end;
+
+        end;
+
+        if(anyCantContinue(results))then
+        begin
+        end
+        else if(anyExceptions(results))then
+        begin
+
+          NSOperationQueue.mainQueue().addOperationWithBlock(method()
+          begin
+            &delegate:OnError(nil);
+          end);
+
+        end
+        else if(anyAuthenticationRequired(results))then
+        begin
+          NSOperationQueue.mainQueue().addOperationWithBlock(method()
+          begin
+            &delegate:OnNotAuthorized(InitiatedActionEnumeration.Startup);
+          end);
+        end
+        else
+        begin
+          NSOperationQueue.mainQueue().addOperationWithBlock(method()
+            begin
+              callback;
+              end);
+
+      end;
+
+      end);
+
+      workerQueue.addOperation(outerExecutionBlock);
+
+    end;
+
 
   end;
 
---
+++ iOSApp.AuthenticatedCore/ServiceDelegates.pas
@@ -0,0 +1,11 @@
+﻿namespace iOSApp.AuthenticatedCore;
+
+uses
+  Foundation;
+
+type
+
+  StartupDelegate = public block ();
+  BuildStartupDelegate = public block ():not nullable NSBlockOperation;
+
+end.
