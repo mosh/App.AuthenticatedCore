88e9f2507a79ec0dcc9970296b30bd5c034c9666
John Moshakis <john.moshakis@firmex.com>
2019-05-05 01:30:40 -0400

    Added claims

---
+++ iOSApp.AuthenticatedCore/Models/Claim.pas
@@ -0,0 +1,20 @@
+﻿namespace iOSApp.AuthenticatedCore.Models;
+
+uses
+  Foundation,
+  iOSApp.Core.Storage.Data;
+
+type
+
+  Claim = public class(DataObject)
+  public
+    property UserId:Integer;
+    property Name:NSString;
+    property Id:Integer;
+    class method primaryKey: NSString; override;
+    begin
+      exit 'Id';
+    end;
+  end;
+
+end.
---
+++ iOSApp.AuthenticatedCore/Models/ClaimTypes.pas
@@ -0,0 +1,10 @@
+﻿namespace iOSApp.AuthenticatedCore.Models;
+
+type
+  ClaimTypes = public class
+  public
+    class const Id: String = 'Id';
+    class const UserName: String = 'UserName';
+  end;
+
+end.
